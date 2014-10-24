57fd0269643f53021c35e8addd46b24c7bf9bf38
Hackbright Student <info@hackbrightacademy.com>
2014-10-23 18:02:30 -0700

    Built movie rating database, added users

--- model.py
+++ model.py
@@ -1,10 +1,49 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import sessionmaker
 
+ENGINE= None
+Session= None
 Base = declarative_base()
 
 ### Class declarations go here
+class User(Base):
+    __tablename__="users"
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(64), nullable=True)
+    password =  Column(String(64), nullable=True)
+    age = Column(Integer, nullable=True)
+    zipcode =  Column(String(15), nullable= True) 
+
+
+class Movie(Base):
+    __tablename__="movies"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=True)
+    released_at = Column(Integer, nullable= True)
+    imdb_url = Column(String(56), nullable = True)
+
+
+class Rating(Base):
+    __tablename__="ratings"
+
+    id = Column(Integer, primary_key= True)
+    movie_id= Column(Integer, nullable=True)
+    user_id = Column(Integer, nullable=True)
+    rating = Column(Integer, nullable=True)
+
+# how can you search using sql in python  without running sqlite3? can you?
+def connect():
+    global ENGINE
+    global Session
+
+    ENGINE = create_engine("sqlite:///ratings.db", echo=True)
+    Session = sessionmaker(bind=ENGINE)
+
+    return Session()
 
 
 ### End class declarations
--- seed.py
+++ seed.py
@@ -2,12 +2,28 @@ import model
 import csv
 
 def load_users(session):
-    # use u.user
-    pass
+    with open('seed_data/u.user', 'rb') as csvfile:
+        user_table = csv.reader(csvfile, delimiter='|')
+        for row in user_table:
+            # delete by hand or add in 
+            # check and see if there's an ID in the database for the data:
+            # if so, don't do anything, if not, add a row 
+            #if id == "is null":
+            new_user = model.User(id = row[0], age = row[1], zipcode = row[4]) 
+            session.add(new_user)
+        session.commit()
+            # print ' '.join(row)
+
+    print user_table 
 
 def load_movies(session):
     # use u.item
-    pass
+    with open('seed_data/u.item', 'rb') as csvfile:
+        item_table= csv.reader(csvfile, delimiter='|')
+        for row in item_table:
+            new_item= model.Movie(id = row[0], name=row[1], released_at= row[2], imdb_url=row[3])
+            session.add(new_item)
+        session.commit()
 
 def load_ratings(session):
     # use u.data
@@ -15,7 +31,10 @@ def load_ratings(session):
 
 def main(session):
     # You'll call each of the load_* functions with the session as an argument
-    pass
+    load_users(session)
+
+
+
 
 if __name__ == "__main__":
     s= model.connect()

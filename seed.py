f5fb64cff539ebabd1bb3efc620754158644660b
Hackbright Student <info@hackbrightacademy.com>
2014-10-27 15:57:27 -0700

    Parsed movie titles and fixed problem with reading in IMDB URLs

--- seed.py
+++ seed.py
@@ -11,7 +11,7 @@ def load_users(session):
             #if id == "is null":
             new_user = model.User(id = row[0], age = row[1], zipcode = row[4]) 
             session.add(new_user)
-        # session.commit()
+        
             # print ' '.join(row)
 
     print user_table 
@@ -21,10 +21,16 @@ def load_movies(session):
     with open('seed_data/u.item', 'rb') as csvfile:
         item_table= csv.reader(csvfile, delimiter='|')
         for row in item_table:
-            new_item= model.Movie(id = row[0], name=row[1].decode("latin-1"), released_at= row[2], imdb_url=row[3])
-            session.add(new_item)
-        # session.commit()
+            movie_title = row[1].split()
+            for i, words in enumerate(movie_title):
+                if words.isalpha() != True:
+                    movie_title.pop(i)
+                movie_string = ' '.join(movie_title)
+                movie_string = movie_string.decode("latin-1")
 
+            new_item= model.Movie(id = row[0], name=movie_string, released_at= row[2], imdb_url=row[4])
+            session.add(new_item)
+     
         print item_table
 
 def load_ratings(session):
@@ -34,14 +40,15 @@ def load_ratings(session):
             new_rating= model.Rating(user_id = row[0], movie_id = row[1], rating = row[2])
             session.add(new_rating)
         
-        session.commit()
-        print ratings_table
-
+       
+        
 def main(session):
     # You'll call each of the load_* functions with the session as an argument
-    # load_users(session)
-    # load_movies(session)
+    load_users(session)
+    load_movies(session)
     load_ratings(session)
+
+    session.commit()
 
 
 

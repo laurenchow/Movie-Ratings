87ff2e5065041750938d053433da0fa270b57480
Hackbright Student <info@hackbrightacademy.com>
2014-10-30 16:01:39 -0700

    Updated to add new ratings for movie

--- judgement.py
+++ judgement.py
@@ -12,9 +12,11 @@ app.jinja_env.undefined = jinja2.StrictUndefined
 
 @app.route('/')
 def index():
-    user_list = model.session.query(model.User).limit(5).all()
-    return render_template("user_list.html", users=user_list)
 
+    return "Welcome to my flashy movie ratings app. Would you like to log in?"
+
+    # user_list = model.session.query(model.User).limit(5).all()
+    # return render_template("user_list.html", users=user_list)
 
 
 
@@ -29,14 +31,18 @@ def user_signup():
 def signup_user():
     user_email = request.form['email']
     user_password = request.form['password']
-    user_age = request.form ['age']
-    user_zipcode = request.form ['zipcode']
+    current_user = model.session.query(model.User).filter_by(email = user_email).first()
+    current_user_id = current_user.id
+
+
     if "login" in session: #this adds users to session
-        session["login"][user_email] = user_password 
+        session["login"][current_user_id] = user_email
     else:
-        session["login"] = {user_email: user_password}
+        session["login"] = {current_user_id: user_email}
+
+    print session
  
-    new_user = model.User(email = user_email, password = user_password, age=user_age, zipcode=user_zipcode)
+    new_user = model.User(email = user_email, password = user_password)
     model.session.add(new_user)
     model.session.commit() 
  
@@ -53,10 +59,6 @@ def show_login():
         return login_user()
 
 def login_user():
-    #check DB - if they are in DB, they can log in, if they are no in DB, then sign up -or login failed   
-    # use hashing here -- different than hashing or hashmap, use MD5 or SHA1 to store the password in the session
-    #before you evaluate password, hash it (use salting too)
-    #check database to see if this user record matches what you have in DB
 
     
     user_email = request.form['email']
@@ -68,7 +70,7 @@ def login_user():
     #if they are in DB, it checks to see if they're in session -if not, it adds them
     if verify_user == []:
         return redirect("/signup")
-
+        # fix this to have user ID instead of email/pw
     else: 
         if "login" in session:
             session["login"][user_email] = user_password 
@@ -78,35 +80,71 @@ def login_user():
         print session
         return redirect("/")
             
+@app.route('/logout')
+def logout_user():
+    pass
+
 @app.route('/viewallusers')
 def view_all_users(): 
     user_list = model.session.query(model.User).limit(50).all()
     return render_template("user_list.html", users=user_list)
 
-# we need to figure out how we pass a single user to this
+
+
 @app.route('/user/<int:id>')
 def view_single_user(id):
     single_user = model.session.query(model.Rating).filter_by(user_id = id).all()
-    # print single_user
-    # print len(single_user)
-    # print "Here's what single_user is  %r" % type(single_user)
-    # print "Here's the ID for single_user %r" % single_user[0].id 
-    # print "Here's what happens if we unpack single_user"  
-    # for item in single_user:
-    #     print item.id
-    #     print item.rating
     return render_template("user.html", user = single_user)
 
-# We should be able to view a list of all users
-# We should be able to click on a user and view the list of movies they've rated, as well as the ratings
-# We should be able to, when logged in and viewing a record for a movie, either add or update a personal rating for that movie.
-
-@app.route('/movie')
-def view_single_movie():
-    return render_template("movie.html")
 
 
-# the list of movies they've rated, as well as the ratings
+@app.route('/movie/<int:id>', methods=['GET', 'POST'])
+def view_single_movie(id):
+    # single_movie = model.session.query(model.Movie).filter_by(id = id).all()
+    single_movie = model.session.query(model.Movie).get(id)
+    print single_movie.ratings
+
+    return render_template("movie.html", film = single_movie)
+
+
+
+@app.route('/add_rating', methods=['GET', 'POST'])
+def show_rating():
+    if request.method == "GET":
+        return render_template("add_rating.html")
+    else:
+        return add_your_own_rating()
+
+def add_your_own_rating():
+    my_own_rating = request.form['my_rating']
+
+
+    print "Here's the rating %r" % my_own_rating
+    print "Here's what is in session %r" % session["login"].keys()
+    current_user_email = session["login"].keys() 
+    #add in a log out function this way you can clear session and only have one 
+    #store user ID in session too as USER id
+    #key: login, value is user ID
+    #will this work if you have multiple users in session? how to figure out which one is logged in?
+    current_user_email = current_user_email[0].rstrip()    
+    print "Here's the stripped user email %r" % current_user_email
+    current_user = model.session.query(model.User).filter_by(email = current_user_email).one()
+    print "Here's what is stored as current user %r" % current_user
+    print "User ID %r" % current_user.id
+    # print "Here's the user ID %r" % model.session.query(model.User).get(current_user) 
+    return "add rating function working"
+
+    # new_rating = model.Rating(current_user)
+    # model.session.add(new_rating)
+    # model.session.commit() 
+ 
+
+
+    
 if __name__ == "__main__":
     app.debug = True
     app.run()
+
+    #check DB - if they are in DB, they can log in, if they are no in DB, then sign up -or login failed   
+    # use hashing here -- different than hashing or hashmap, use MD5 or SHA1 to store the password in the session
+    #check database to see if this user record matches what you have in DB
